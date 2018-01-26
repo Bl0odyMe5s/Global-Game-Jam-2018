6c0931e1a7fba210afd2242426eca5d6cd42d5ac
Michael van der Raad <michael.van.der.raad@hva.nl>
2018-01-27 00:32:38 +0100

    Detect when a ball passes user

--- Assets/Scripts/ObjectBehaviours/Ball.cs
+++ Assets/Scripts/ObjectBehaviours/Ball.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	public float gravity = 0.4f;
+	public float startForce = 300f;
+	public float playerYMargin;
+
+	private float playerY = 0;
+
 	// Use this for initialization
 	void Start () {
-		Physics.gravity = Vector3.down * 0.4f;
+		Physics.gravity = Vector3.down * gravity;
 
-		GetComponent<Rigidbody> ().AddForce (Vector2.down * 150f, ForceMode.Impulse);
+		//Get player Y position from game manager
+		Transform player = GameObject.Find ("player").transform;
+		playerY = player.position.y;
+
+		GetComponent<Rigidbody> ().AddForce (Vector2.down * startForce, ForceMode.Impulse);
+	}
+
+	private void FixedUpdate()
+	{
+		if (transform.position.y >= playerY + playerYMargin) {
+			gameObject.SetActive (false);
+			Debug.Log ("JIJ BENT AF!");
+		}
 	}
 }
