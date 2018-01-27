6761a35c981252ff5d306d8a65d080bb393be213
Michael van der Raad <michael.van.der.raad@hva.nl>
2018-01-27 14:13:24 +0100

    Add falling tiles

---
+++ Assets/Scripts/ObjectBehaviours/MapController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour {
+
+	public int tileCount = 0;
+	public float releaseProbability = 0;
+
+    [SerializeField]
+    private Transform partParent;
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		if(Random.Range(0, 100) > 100f - releaseProbability)
+        {
+            ReleaseTile();
+        }
+	}
+
+	public void ReleaseTile()
+	{
+        int randomIndex = Random.Range(0, partParent.childCount);
+
+        int index = 0;
+        foreach(Transform child in partParent)
+        {
+            if (index == randomIndex)
+            {
+                // Release tile
+                child.parent = null;
+                Rigidbody rb = child.gameObject.AddComponent<Rigidbody>();
+
+                return;
+            }
+
+            index++;
+        }
+	}
+}
