8aba29cd485e1011a840a9416a1ed6724cba5856
Richard Weug <richardweug@live.nl>
2018-01-28 04:41:39 +0100

    Add screen shake on hit (forgot scripts?)

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    private float _shakeDuration;
+	
+    private const float ShakeAmount = 0.7f;
+    private const float DecreaseFactor = 1.0f;
+	
+    private Vector3 _originalPosition;
+
+    private void Start()
+    {
+        _originalPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_shakeDuration > 0)
+        {
+            transform.localPosition = _originalPosition + Random.insideUnitSphere * ShakeAmount;
+            _shakeDuration -= Time.deltaTime * DecreaseFactor;
+        }
+        else
+        {
+            _shakeDuration = 0f;
+            transform.localPosition = _originalPosition;
+        }
+    }
+    
+    public void Shake(float shakeSeconds)
+    {
+        _shakeDuration = shakeSeconds;
+    }
+}
