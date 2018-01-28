f5969f589c748decaa952df85b477a6e0d694d97
Michael van der Raad <michael.van.der.raad@hva.nl>
2018-01-28 01:47:42 +0100

    Add drone and claw

--- Assets/Scripts/Managers/Manager.cs
+++ Assets/Scripts/Managers/Manager.cs
@@ -91,6 +91,13 @@ public class Manager : MonoBehaviour
         int random = (int)Mathf.Round(Random.Range(0, 2));
         spawnPoints.Add(GameObject.Find("Ball Spawn 1").transform.position);
         spawnPoints.Add(GameObject.Find("Ball Spawn 2").transform.position);
+        
+        if(newState == GameStates.Playing)
+        {
+            Ball.GetComponent<Rigidbody>().isKinematic = true;
+            Ball.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
         Ball.transform.position = spawnPoints[random];
         BallScript.PlayerY = player.transform.position.y;
         state = newState;
